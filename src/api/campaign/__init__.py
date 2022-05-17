435c115d3e3d66b02bc94f97c1f6142d3f16fa5a
Pham Anh Tai <phamanhtai0410@gmail.com>
2022-05-17 23:44:57 +0700

    _stag_2_ add url rule for api

--- src/api/campaign/__init__.py
+++ src/api/campaign/__init__.py
@@ -6,7 +6,14 @@
 """
 from flask import Blueprint
 
-from .controller import 
+from .controller import *
 
 rest_campaign = Blueprint('rest_campaign', __name__, url_prefix='')
-rest_campaign.add_url_rule('', methods=[''], view_func=)
+
+rest_campaign.add_url_rule('create_new', methods=['POST'], view_func=create_new_campaign)
+
+rest_campaign.add_url_rule('get_list', methods=['GET'], view_func=list_campaigns)
+
+rest_campaign.add_url_rule('edit_non_released', methods=['POST'], view_func=edit_non_released_campaign)
+
+rest_campaign.add_url_rule('edit_released', methods=['POST'], view_func=edit_released_campaign)
