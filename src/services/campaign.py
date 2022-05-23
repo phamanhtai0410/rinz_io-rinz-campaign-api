1c2fbbe20861cec7e8f30b8599eba98c768e4672
Pham Anh Tai <phamanhtai0410@gmail.com>
2022-05-23 15:35:39 +0700

    _stag_22_ fix query error

--- src/services/campaign.py
+++ src/services/campaign.py
@@ -89,12 +89,12 @@ class CampaignService(object):
     @classmethod
     def edit_release_campaign(cls, user_id, campaign_id, edit_infos):
         
-        _campaign = CampaignModel.get_item(oid=campaign_id).to_dict()
+        _campaign = CampaignModel.get_item(oid=campaign_id)
         
         if _campaign['user'] != user_id:
             raise ExCampaign("Not have permission to update this campaign !")
 
-        if not _campaign['is_realeased']:
+        if not _campaign['is_released']:
             raise ExCampaign("This campaign's not a released one !")
 
         CampaignModel.update_one(
@@ -105,8 +105,6 @@ class CampaignService(object):
         )
 
         return campaign_id, 'success'
-
-
 
     @classmethod
     def get_list_campaigns(cls, _user_id, page=CampaignGetList.DEFAULT_PAGE, page_size=CampaignGetList.DEFAULT_PAGE_SIZE):
@@ -126,15 +124,15 @@ class CampaignService(object):
 
     @classmethod
     def release_campaign(cls, campaign_id, user_id):
-        _campaign = CampaignModel.get_item(oid=campaign_id).to_dict()
+        _campaign = CampaignModel.get_item(oid=campaign_id)
 
         if _campaign['is_released']:
             raise ExCampaign("This campaign 's already released !")
         
         if _campaign['user'] != user_id:
-            raise ExCampaign("Not have permissions to release this campagn !")
+            raise ExCampaign("Not have permissions to release this campaign !")
         
-        ###     Create submodomain for campaign 
+        #    Create subdomain for campaign
         #       @params: subdomain need to be created
         #       @return: result of creation: True or False and created subdomain 
 
@@ -144,18 +142,21 @@ class CampaignService(object):
         )
         # print("*** Subdomain Creation Result : ", _creation_result)
 
-        ###     Update released status in db record  
+        #       Update released status in db record
         #       @params: None
-        #       @return: update field: "is_released": True     
-        CampaignModel.update(
-            oid=campaign_id,
+        #       @return: update field: "is_released": True
+
+        CampaignModel.update_one(
+            filter={
+                "_id": ObjectId(campaign_id)
+            },
             obj={
                 "is_released": True
             }
         )
 
 
-        ###     Call to CampaignFactory to deploy new campaign contract
+        #     Call to CampaignFactory to deploy new campaign contract
         #       params: infors of campaigns
         #       return: created campaign contract's address
         
